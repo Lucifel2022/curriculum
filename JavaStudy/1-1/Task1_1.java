fef9b5dc6759db26d2e88563425ec0229fa808b1
Lucifel2022 <99029339+Lucifel2022@users.noreply.github.com>
2022-02-08 23:28:41 +0900

    Create Task1_1.java

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,29 @@
+public class Task1_1 {
+
+    public static void main(String[] args) {
+//①
+int hogeint = 50 ;
+
+//②
+String hogestring = "テスト" ;
+
+//③Double型のhogeDoubleという変数を宣言しその値を「0.2」で初期化
+Double hogeDouble = 0.2 ;
+
+//④
+hogeint = 100 ;
+
+//⑤
+String hogestiring = "合格" ;
+
+//⑥
+System.out.println(hogeint);
+
+//⑦上書きされたhogestringの値を改行して表示
+System.out.println(hogestring);
+
+//⑧
+System.out.println(hogeDouble);
+
+    }
+}
