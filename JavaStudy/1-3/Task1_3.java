daea3c80225982d3e6559cee70fdbf68e5b3190a
Lucifel2022 <99029339+Lucifel2022@users.noreply.github.com>
2022-02-11 20:35:16 +0900

    Create Task1_3.java

---
+++ JavaStudy/1-3/Task1_3.java
@@ -0,0 +1,36 @@
+public class Task1_3{
+  
+  public static void main(String[] args){
+//①
+int plus = 5 + 10;
+
+System.out.println(plus);
+
+//②
+int minus = 20 - 7;
+
+System.out.println(minus);
+
+//③
+//10と2を乗算した値を代入する変数multiplyを作成。
+int multiply = 10 * 2;
+
+System.out.println(multiply);
+
+//④
+int remi = 20 % 6;
+
+System.out.println(remi);
+
+//⑤
+//Stringクラスを宣言して、変数名はtownとlineで
+//townには「有楽町」lineには「線」で初期化
+String town = "有楽町";
+String line = "線";
+
+String train = (town+line);
+
+System.out.println(train);
+
+  }
+}
