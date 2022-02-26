eaa2530182d93344579a8733f2e0e64db7c30f20
Lucifel2022 <99029339+Lucifel2022@users.noreply.github.com>
2022-02-27 00:26:42 +0900

    2-1

---
+++ JavaStudy/chapter1/src/chapter1/Ex1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package chapter1;
+
+/**
+ * @author lucif
+ *
+ */
+public class Ex1 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        System.out.println("hello java");
+    ｝
+｝
+
