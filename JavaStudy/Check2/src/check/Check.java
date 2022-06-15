4b873b4863b2f72a655b121c79533571f11b1b74
Lucifel2022 <99029339+Lucifel2022@users.noreply.github.com>
2022-06-15 22:36:06 +0900

    再提出Check2

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,5 +1,5 @@
 package check;
-
+import constants.Constants;
 public class Check {
    private static String firstName = "片野" ;
    private static String lastName =  "司" ;
@@ -11,8 +11,8 @@ public class Check {
    }
     public static void main(String[] args) {
         System.out.println("printNameメソッド　→　"+getName(firstName , lastName));
-    Pet pet = new Pet("java吉" , "hoge"); 
-        RobotPet robot = new RobotPet("R2D2" , "ルーク");
+    Pet pet = new Pet(Constants.CHECK_CLASS_JAVA , Constants.CHECK_CLASS_HOGE); 
+        RobotPet robot = new RobotPet(Constants.CHECK_CLASS_R2D2 ,Constants.CHECK_CLASS_LUKE );
       pet.introduce();
      robot.introduce();
     }
